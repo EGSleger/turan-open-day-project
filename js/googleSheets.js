204b7b2ba9b335ac89d039398ac0de89cdbfdddf
Yegor Kovnet <91253328+EGSleger@users.noreply.github.com>
2024-06-13 21:14:07 +0500

    Add files via upload

---
+++ js/googleSheets.js
@@ -0,0 +1,38 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbymHzVE-YSounyfP0L6xh5Td4k5fHT0S9sMpD9XeCLAFiIBH3T4hLYXV0LUyv13pGNc/exec';
+        const form = document.forms['submit-to-google-sheet'];
+        const openModelCard1 = document.getElementById("open_model1");
+        const openModelCard2 = document.getElementById("open_model2");
+        const openModelCard3 = document.getElementById("open_model3");
+
+        let clickedCardData = null;
+
+        openModelCard1.addEventListener('click', () => {
+            clickedCardData = 'Card 1';
+        });
+
+        openModelCard2.addEventListener('click', () => {
+            clickedCardData = 'Card 2';
+        });
+
+        openModelCard3.addEventListener('click', () => {
+            clickedCardData = 'Card 3';
+        });
+
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+                .then(response => {
+                    console.log('Form submitted successfully!', response);
+                    if (clickedCardData) {
+                        const formData = new FormData();
+                        formData.append('data', clickedCardData);
+                        return fetch(scriptURL, { method: 'POST', body: formData });
+                    }
+                })
+                .then(response => {
+                    if (response) {
+                        console.log('Card data sent successfully!', response);
+                    }
+                })
+                .catch(error => console.error('Error!', error.message));
+        });
---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("open_model1").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("open")
+})
+document.getElementById("open_model2").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("open")
+})
+document.getElementById("open_model3").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("open")
+})
+
+document.getElementById("close_btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.remove("open")
+})
+
+document.getElementById("reg_btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.remove("open")
+})
