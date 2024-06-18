7d2d2bd6bea04c37b9de62652a503f69d66a695d
Yegor Kovnet <91253328+EGSleger@users.noreply.github.com>
2024-06-18 23:33:28 +0500

    Update googleSheets.js

--- js/googleSheets.js
+++ js/googleSheets.js
@@ -7,32 +7,29 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbymHzVE-YSounyfP0L6xh
         let clickedCardData = null;
 
         openModelCard1.addEventListener('click', () => {
-            clickedCardData = '25 апреля';
+            clickedCardData = '25 Апреля';
         });
 
         openModelCard2.addEventListener('click', () => {
-            clickedCardData = '16 мая';
+            clickedCardData = '16 Мая';
         });
 
         openModelCard3.addEventListener('click', () => {
-            clickedCardData = '9 июня';
+            clickedCardData = '9 Июня';
         });
 
         form.addEventListener('submit', e => {
             e.preventDefault();
-            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-                .then(response => {
-                    console.log('Form submitted successfully!', response);
-                    if (clickedCardData) {
-                        const formData = new FormData();
-                        formData.append('data', clickedCardData);
-                        return fetch(scriptURL, { method: 'POST', body: formData });
-                    }
-                })
-                .then(response => {
-                    if (response) {
-                        console.log('Card data sent successfully!', response);
-                    }
+            const formData = new FormData(form);
+            
+            if (clickedCardData) {
+                formData.append('data', clickedCardData);
+            }
+
+            fetch(scriptURL, { method: 'POST', body: formData })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Form and card data submitted successfully!', data);
                 })
                 .catch(error => console.error('Error!', error.message));
         });
